2f8c626ced62be72b55a863c51f7c15490f580cd
Viral Patel <viral.patel@1rivet.local>
2023-07-29 23:59:52 +0530

    feat: completedhomepage without responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,39 @@
-$(document).ready(function() {
-    $("#user-menu").click(function() {
+$(document).ready(function () {
+    $("#user-menu").click(function () {
         $("#user-dropdown").fadeToggle(150);
         $("#lang-dropdown").hide();
     });
-    
-    $("#selected-lang").click(function() {
+
+    $("#selected-lang").click(function () {
         $("#lang-dropdown").fadeToggle(150);
         $("#user-dropdown").hide();
+    });
+
+    $("#slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+
+    $("#slider-2").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+    
+    $("#slider-3").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+    
+    $("#slider-4").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
     });
 });
