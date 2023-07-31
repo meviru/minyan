673f8906b482151f9bc530798393e941876dc481
Viral Patel <viral.patel@1rivet.local>
2023-07-31 18:25:44 +0530

    feat: responsive design changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,27 +13,91 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $("#slider-2").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
-    
+
     $("#slider-3").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
-    
+
     $("#slider-4").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 });
